fc34104978514398274189f86229f07ccaafdf94
Aedan Cullen <aedancullen@gmail.com>
2017-01-31 17:45:51 -0500

    Add more backend code

--- python/src/awareness/backend.py
+++ python/src/awareness/backend.py
@@ -4,3 +4,22 @@ import algorithm as i_algorithm
 import data as i_data
 import endpoint as i_endpoint
 import protocol as i_protocol
+
+class Backend:
+
+    @abstractmethod
+    def async(self):
+        pass
+
+    @abstractmethod
+    def send(self):
+        pass
+
+    @abstractmethod
+    def receive(self):
+        pass
+
+
+class NativeBackend(Backend):
+
+    pass
