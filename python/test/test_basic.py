0581ad4eeff6d0e410d527d6049c65e9ffd9e7c0
Aedan Cullen <aedancullen@gmail.com>
2017-02-18 10:36:27 -0500

    Add correct sizing features

--- python/test/test_basic.py
+++ python/test/test_basic.py
@@ -3,9 +3,11 @@ import awareness
 
 class TestAbility(awareness.ability.LocalAbility):
     def run(self, input):
-        return input + 1
+        output = [None] * self.outputs
+        output[0] = input[0] + 1
+        return output
 
 def test_localability():
     endp = awareness.endpoint.LocalEndpoint()
     endp.abilities.append(TestAbility(endp, 0, 1, 1))
-    assert endp.processData(0, 4) == 5
+    assert endp.processData(0, [4]) == [5]
