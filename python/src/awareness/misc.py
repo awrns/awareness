afc5bd6d42083aadde9262c79c402a625435f51c
Aedan Cullen <aedancullen@gmail.com>
2017-03-10 16:32:48 +0000

    Add ProgressingResult

--- python/src/awareness/misc.py
+++ python/src/awareness/misc.py
@@ -3,3 +3,20 @@ import backend as i_backend
 import data as i_data
 import operator as i_operator
 import protocol as i_protocol
+
+
+class ProgressingResult:
+
+    result = None
+    stopping = False
+
+    def progressCallback(self, result):
+        self.result = result
+        return self.stopping
+
+    def stop(self):
+        self.stopping = True
+
+
+
+
