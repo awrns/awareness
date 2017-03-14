8960eed6a07574e149d669c4756b45d2e0aab129
Aedan Cullen <aedancullen@gmail.com>
2017-03-13 21:40:29 -0400

    Re-add status PDU

--- python/src/awareness/misc.py
+++ python/src/awareness/misc.py
@@ -13,14 +13,16 @@ class Protocol0Constants:
     BLANK =                 0x01
 
     SEARCH_CAPABILITIES =   0x10
-    SEARCH_TASK_PARAMS =         0x11
+    SEARCH_TASK_PARAMS =    0x11
     SEARCH_TASK_START =     0x12
     SEARCH_TASK_STOP =      0x13
+    SEARCH_TASK_STATUS =    0x14
 
     PROCESS_CAPABILITIES =  0x20
-    PROCESS_TASK_PARAMS =        0x21
+    PROCESS_TASK_PARAMS =   0x21
     PROCESS_TASK_START =    0x22
     PROCESS_TASK_STOP =     0x23
+    PROCESS_TASK_STATUS =   0x24
 
     UNIT_ERROR =            0x30
     DATA_ERROR =            0x31
@@ -32,11 +34,13 @@ class Protocol0Constants:
     searchTaskParamsPreStruct =         struct.Struct("!")
     searchTaskStartPreStruct =          struct.Struct("!")
     searchTaskStopPreStruct =           struct.Struct("!")
+    searchTaskStatusPreStruct =         struct.Struct("!")
 
     processCapabilitiesPreStruct =      struct.Struct("!")
     processTaskParamsPreStruct =        struct.Struct("!")
     processTaskStartPreStruct =         struct.Struct("!")
     processTaskStopPreStruct =          struct.Struct("!")
+    processTaskStatusPreStruct =         struct.Struct("!")
 
     unitErrorPreStruct =                struct.Struct("!")
     dataErrorPreStruct =                struct.Struct("!")
@@ -48,11 +52,13 @@ class Protocol0Constants:
     searchTaskParamsDatumStruct =       struct.Struct("!")
     searchTaskStartDatumStruct =        struct.Struct("!")
     searchTaskStopDatumStruct =         struct.Struct("!")
+    searchTaskStatusDatumStruct =       struct.Struct("!")
 
     processCapabilitiesDatumStruct =    struct.Struct("!")
     processTaskParamsDatumStruct =      struct.Struct("!")
     processTaskStartDatumStruct =       struct.Struct("!")
     processTaskStopDatumStruct =        struct.Struct("!")
+    processTaskStatusDatumStruct =      struct.Struct("!")
 
     unitErrorDatumStruct =              struct.Struct("!")
     dataErrorDatumStruct =              struct.Struct("!")
@@ -63,10 +69,12 @@ class Protocol0Constants:
                       SEARCH_TASK_PARAMS: searchTaskParamsPreStruct,
                       SEARCH_TASK_START: searchTaskStartPreStruct,
                       SEARCH_TASK_STOP: searchTaskStopPreStruct,
+                      SEARCH_TASK_STATUS: searchTaskStatusPreStruct,
                       PROCESS_CAPABILITIES: processCapabilitiesPreStruct,
                       PROCESS_TASK_PARAMS: processTaskParamsPreStruct,
                       PROCESS_TASK_START: processTaskStartPreStruct,
                       PROCESS_TASK_STOP: processTaskStopPreStruct,
+                      PROCESS_TASK_STATUS: processTaskStatusPreStruct,
                       UNIT_ERROR: unitErrorPreStruct,
                       DATA_ERROR: dataErrorPreStruct}
 
@@ -76,9 +84,11 @@ class Protocol0Constants:
                         SEARCH_TASK_PARAMS: searchTaskParamsDatumStruct,
                         SEARCH_TASK_START: searchTaskStartDatumStruct,
                         SEARCH_TASK_STOP: searchTaskStopDatumStruct,
+                        SEARCH_TASK_STATUS: searchTaskStatusDatumStruct,
                         PROCESS_CAPABILITIES: processCapabilitiesDatumStruct,
                         PROCESS_TASK_PARAMS: processTaskParamsDatumStruct,
                         PROCESS_TASK_START: processTaskStartDatumStruct,
                         PROCESS_TASK_STOP: processTaskStopDatumStruct,
+                        PROCESS_TASK_STATUS: processTaskStatusDatumStruct,
                         UNIT_ERROR: unitErrorDatumStruct,
                         DATA_ERROR: dataErrorDatumStruct}
