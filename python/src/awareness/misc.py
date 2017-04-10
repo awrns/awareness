044f2501dd32043a92a4e01b672f5600bdb6ba9d
Aedan Cullen <aedancullen@gmail.com>
2017-04-10 15:52:47 +0000

    Finish search task status datum struct

--- python/src/awareness/misc.py
+++ python/src/awareness/misc.py
@@ -61,7 +61,7 @@ class Protocol0Constants:
     capabilities_datum_struct =             struct.Struct("!2Q")     		# inputs, outputs
 
     search_task_start_datum_struct =        struct.Struct("!d")	      	 	# input_set data items(s)
-    search_task_status_datum_struct =       struct.Struct("!64sH2Q") 	   	# host, port, slice, offset, remap
+    search_task_status_datum_struct =       struct.Struct("!64sH2Q1024s") 	# host, port, slice, offset, remap
     search_task_stop_datum_struct =         struct.Struct("!")	     		# intentional
 
     process_task_start_datum_struct =       struct.Struct("!d")	    		# input Stream data item(s)
