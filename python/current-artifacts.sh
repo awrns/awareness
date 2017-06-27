b3b4f8924ae951605ba8744b26929d174c51bc5e
Aedan Cullen <aedancullen@gmail.com>
2017-06-27 10:50:41 -0400

    Update artifacts

---
+++ python/current-artifacts.sh
@@ -0,0 +1,1 @@
+java -jar ~/pygradle/build/pivy-importer/libs/pivy-importer-0.5.8-SNAPSHOT-all.jar --repo artifacts Theano:0.9.0 --replace numpy:1.9.1=numpy:1.13.0,scipy:0.14.0=scipy:0.19.1,six:1.9.0=six:1.10.0
