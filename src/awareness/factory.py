f60750beb8874b7b1ac0fbd2ed6927e340ce38f8
Aedan Cullen <aedancullen@gmail.com>
2017-07-25 13:28:41 -0400

    Revert "Add Keras dependency"
    This reverts commit 9c86cfe1d4d391acbee5764ca1f8b63f0d92c8ef.

--- src/awareness/factory.py
+++ src/awareness/factory.py
@@ -16,20 +16,41 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Awareness.  If not, see <http://www.gnu.org/licenses/>.
 #
+# Portions of this file are originally the work of Michael Nielsen,
+# and are distributed here in a modified form under the terms of the MIT License.
+# The following information applies to these portions.
+#
+# Copyright (c) 2012-2015 Michael Nielsen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a 
+# copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the Software 
+# is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included 
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from abc import ABCMeta, abstractmethod
 
+import io
+import sys
 
-class Factory(metaclass=ABCMeta):
+backup = sys.stderr
+sys.stderr = io.StringIO()
+import theano
+out = sys.stderr.getvalue()
+sys.stderr = backup
 
-    @abstractmethod
-    def fabricate(self, input_set, progress_frequency=0, progress_callback=None):
-        raise NotImplementedError()
+if out.find("(") != -1:
+    out = out[:out.find("(")]
 
-
-class DefaultFactory(Factory):
-
-
-    def fabricate(self, input_set, progress_frequency=0, progress_callback=None):
-        pass
-
+sub = out.split(":")[-1]
+device = sub.strip() if sub.strip() != "" else "CPU"
