34754b23ca27ae42a45a711ed3d27536b9f9ee9c
Aedan Cullen <aedancullen@gmail.com>
2017-09-10 12:05:43 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     ],
 
     name='awareness',
-    version='14',
+    version='16',
     description='The new architecture of co-computation for data processing and machine learning.',
     author='Aedan Cullen',
     author_email='aedancullen@gmail.com',
